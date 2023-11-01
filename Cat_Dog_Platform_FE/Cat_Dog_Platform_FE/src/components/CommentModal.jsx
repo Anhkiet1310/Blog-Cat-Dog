222c53aee12101aeef4643c37fcfa23c8364c200
Wizin25 <tanphu200213@gmail.com>
2023-11-01 19:31:29 +0700

    comment and like prototype

--- Cat_Dog_Platform_FE/Cat_Dog_Platform_FE/src/components/CommentModal.jsx
+++ Cat_Dog_Platform_FE/Cat_Dog_Platform_FE/src/components/CommentModal.jsx
@@ -3,17 +3,15 @@ import { accountAtom } from "../atom/accountAtom";
 import api from "../api/api";
 
 export const CommentModal = ({ handleClose, Post }) => {
-  console.log("Post", Post)
   const account = useRecoilValue(accountAtom);
 
   const submit = async () => {
-    const comment = document.querySelector(`input#comment`).value;
-    if (comment?.length>0) {
+    const comment = document.querySelector(`input`);
+    if (comment) {
       const result = await api.uploadComment(
         comment,
-        Post.postId,
         account.accountId,
-        
+        Post.postId
       );
       console.log(result);
       handleClose();
@@ -22,26 +20,22 @@ export const CommentModal = ({ handleClose, Post }) => {
   return (
     <>
       {Post ? (
-        <div className="bg-neutral-900 text-white h-4/5 overflow-y-scroll">
-          <div className="mb-5">
-            <div className="font-medium mb-3">
+        <div className="bg-neutral-900 text-white">
+          <div>
             {account.username}
-            </div>
-            <div className="flex justify-between pr-5">
-            <input id="comment"
-              className="p-2 rounded-full w-2/3 bg-neutral-700"
+            <input
+              className="p-2 rounded-full bg-neutral-700"
               placeholder={`what's your thought ${account.username}`}
             />
-            <button onClick={submit} className="p-2 rounded-2xl bg-blue-600">
+            <button onClick={submit} className="p-2 rounded-2xl">
               Comment
             </button>
-            </div>
           </div>
           <div>
             {Post.postComments.map((comment) => (
               <>
                 <div>
-                  <div>{comment.account.username}</div>
+                  {comment.account.username}
                   {comment.content}
                 </div>
               </>
