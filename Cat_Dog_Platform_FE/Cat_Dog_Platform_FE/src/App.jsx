05d11cc4a53f07bb92c7a93de17509405126e23e
Anhkiet1310 <nguyenvoanhkiet997@gmail.com>
2023-11-05 23:35:04 +0700

    completed functions

--- Cat_Dog_Platform_FE/Cat_Dog_Platform_FE/src/App.jsx
+++ Cat_Dog_Platform_FE/Cat_Dog_Platform_FE/src/App.jsx
@@ -8,11 +8,21 @@ import {
 import PageLayout from "./layouts/PageLayout";
 import "./App.css";
 import "react-quill/dist/quill.snow.css";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 import BlogTypeList from "./pages/Blogs/BlogTypeList";
 import PostList from "./pages/Posts/PostList";
 import BlogDetail from "./pages/Blogs/BlogDetail/BlogDetail";
 import SignInPage from "./pages/Auth/SignInPage";
 import AuthAction from "./pages/Auth/AuthAction";
+import { BlogManager } from "./pages/Admin/BlogManager/BlogManager";
+import { blogManagerLoader } from "./pages/Admin/BlogManager/BlogManagerLoader";
+import { AccountManager } from "./pages/Admin/AccountManager/AccountManager";
+import { accountManagerLoader } from "./pages/Admin/AccountManager/AccountManagerLoader";
+import { Profile } from "./pages/Profile/Profile";
+import { profileLoader } from "./pages/Profile/ProfileLoader";
+import { SearchPage } from "./pages/SearchPage/SearchPage";
+import { searchPageLoader } from "./pages/SearchPage/SearchPageLoader";
 
 const LazyPostList = lazy(() => import("./pages/Posts/PostList"));
 const LazyBlogList = lazy(() => import("./pages/Blogs/BlogList"));
@@ -42,6 +52,11 @@ function App() {
             }
           />
           {/* <Route index element={<PostList />}/> */}
+
+          <Route path="BlogManager" element={<BlogManager />} loader={blogManagerLoader}/>
+          <Route path="AccountManager" element={<AccountManager />} loader={accountManagerLoader}/>
+          <Route path="Profile/:accountId" element={<Profile />} loader={profileLoader}/>
+          <Route path="search/:search" element={<SearchPage />} loader={searchPageLoader}/>
         </Route>
       </Route>
     ),
@@ -50,6 +65,7 @@ function App() {
   return (
     <>
       <RouterProvider router={router} fallbackElement={<h1>Loading...</h1>} />
+      <ToastContainer />
     </>
   );
 }
